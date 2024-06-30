f60c2fec15e7345cf086f381b0363eb29a464b0b
Afsal kp <afsalkp4343@gmail.com>
2024-06-30 22:31:28 +0530

    complete 80% design

--- route/userRouter.js
+++ route/userRouter.js
@@ -10,4 +10,25 @@ UserRouter.get("/",(req,res)=>{
 UserRouter.get('/login',(req,res)=>{
     res.render("user/login.hbs",{log:true})
 })
+UserRouter.get('/register',(req,res)=>{
+    res.render("user/register.hbs",{log:true})
+})
+UserRouter.get('/register/otp',(req,res)=>{
+    res.render("user/otp.hbs",{log:true})
+})
+UserRouter.get('/login/resetpwd',(req,res)=>{
+    res.render('user/resetpwd.hbs')
+})
+UserRouter.get('/cart',(req,res)=>{
+    res.render('user/cart.hbs')
+})
+UserRouter.get('/products',(req,res)=>{
+    res.render('user/products.hbs')
+})
+UserRouter.get('/wishlist',(req,res)=>{
+    res.render('user/wishlist.hbs')
+})
+UserRouter.get('/cart/checkout',(req,res)=>{
+    res.render('user/checkout.hbs')
+})
 module.exports=UserRouter;
