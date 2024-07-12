0715f920ca5ab6b9cc80489ddc56ed5d78c8aae9
Afsal kp <afsalkp4343@gmail.com>
2024-07-12 14:58:24 +0530

    admin page product edit completd

--- controller/admin/productController.js
+++ controller/admin/productController.js
@@ -105,25 +105,87 @@ const Editproduct=async(req,res)=>{
 const newlistedProduct=async(req,res)=>{
 
 try {
+   const image=[]
     console.log(req.body,"body")
     id=req.body.product_id
-
-product=await Product.findById(id).lean();
+    let fileData=req.files.length
+    console.log(req.files.length,"filess")
+let product=await Product.findById(id).lean();
 
 console.log(product,"product")
 console.log(req.body,"body")
 
 const {name,description,Brand,category,price,stock,discount_price}=req.body;
 
-if(product.name==name&&product.description==description&&product.brand==Brand&&product.price==price&&product.stock==stock&&product.discount_price==discount_price){
-    res.send("peoduct already exist")
-}else if(req.files.length==0){
-res.send("already exist")
-}else{
+if(product){
 
-res.send("newa ddproduce")
+    const isEqual = (
+        product.name==name&&
+        product.description == description &&
+        
+         product.brand == Brand&&
+         
+          product.price==price&&
+         product.stock==stock&&
+         product.discount_price==discount_price&&
+       fileData==0
+        
+      );
+
+      console.log(isEqual,"eqial")
+if(isEqual){
+   
+        res.send("imagealredy exist")
+    }
+else{
+
+// imgecrop
+const imageData = [];
+const imageFiles = req.files;
+for(file of imageFiles){
+    imagename=file.filename
+ imagepath=path.join('public','upload')
+console.log(file,"crop")
+const randomInteger = Math.floor(Math.random() * 20000001);
+const imgFileName = "cropped" + randomInteger + ".jpg";
+fullpath=path.join(imagepath,imagename)
+console.log(fullpath,"fullapath")
+cropImage= await sharp(file.path).resize(200,200,{fit:"cover"}).toFile(imagepath+'/'+imgFileName)
+console.log(cropImage,"croped")
+if(cropImage){
+imageData.push(imgFileName)
+}
+}
+//end
+
+
+
+
+    
+    const updateProductData = await Product.findByIdAndUpdate(
+        { _id:id },
+        {
+          $set: {
+            name: name,
+            brand: Brand,
+            category: category,
+            stock: stock,
+            price: price,
+            discount_price: discount_price,
+            description: description,
+            image: imageData
+          },
+        }
+      );
+      await updateProductData.save()
+
+res.redirect('/admin/listproducts')
 
 }
+
+    }
+    
+    else{res.send("product not found in database") }
 
 
 
@@ -136,11 +198,11 @@ res.send("newa ddproduce")
     console.log(error)
     
 }
-
+}
 
 
  
-}
+
 module.exports={
     uploaded,
     addProducts,
